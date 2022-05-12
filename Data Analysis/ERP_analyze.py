4611a2235586ab137e487648be6f7e30d042e2fe
BCI Lab <ergo2.ncsu@gmail.com>
2022-05-12 13:05:07 -0400

    ERP analysis script now properly renames channels using the Channel Index file. Note that these amplifier orders (present in ERP_analyze.py) are subject to change with each participant.

--- Data Analysis/ERP_analyze.py
+++ Data Analysis/ERP_analyze.py
@@ -1,4 +1,6 @@
 import mne
+import string
+import channels
 
 stims = {
     '32769': 'OVTK_StimulationId_ExperimentStart',
@@ -8,13 +10,27 @@ stims = {
 }
 
 montage = mne.channels.make_standard_montage('standard_1020')
-fnames = []
+fnames = ["C:/Users\zatraylo\PycharmProjects\Pain-BCI\Experiment Code\Data\sub1\sub1-attend-1.gdf",
+          "C:/Users\zatraylo\PycharmProjects\Pain-BCI\Experiment Code\Data\sub1\sub1-attend-2.gdf",
+          "C:/Users\zatraylo\PycharmProjects\Pain-BCI\Experiment Code\Data\sub1\sub1-attend-3.gdf",
+          "C:/Users\zatraylo\PycharmProjects\Pain-BCI\Experiment Code\Data\sub1\sub1-attend-4.gdf",
+          "C:/Users\zatraylo\PycharmProjects\Pain-BCI\Experiment Code\Data\sub1\sub1-attend-5.gdf",
+          "C:/Users\zatraylo\PycharmProjects\Pain-BCI\Experiment Code\Data\sub1\sub1-attend-6.gdf"]
+
+amp_order = sum([channels.labels["UB20160321"], channels.labels["UB20141008"],  # order amplifiers as OpenViBE did
+                 channels.labels["UB20141007"], channels.labels["UB20110715"]], [])  # (see subject summaries)
+
 epochs_list = []
-ts = -0.3  # epoch start time
-te = 0.7  # epoch end time
+ts = -0.1  # epoch start time
+te = 0.5  # epoch end time
 
 for f in fnames:
     raw = mne.io.read_raw_gdf(f, preload=True)  # load our GDF file
+
+    # Correct channel names using amp_order
+    channel_names = {raw.ch_names[i]: amp_order[i] for i in range(len(amp_order))}  # both should be of length 64
+    raw.rename_channels(channel_names)  # happens in place
+
     raw.set_montage(montage)  # add standard 10-20 montage information to GDF file
 
     # Pull the event markers from the data's annotations
@@ -25,19 +41,18 @@ for f in fnames:
     for event in events:
         event[0] = event[0] - offset
 
-    raw.filter(l_freq=0.5, h_freq=10.0)  # bandpass filter 0.5Hz-10Hz
-    raw.notch_filter(60.0)  # might not be needed considering tight bandpass above...
+    raw.filter(l_freq=0.1, h_freq=10.0)  # bandpass filter 0.5Hz-10Hz
 
     epochs_list.append(mne.Epochs(raw, events, event_id=temp_id, tmin=ts, tmax=te,
                                   preload=True, baseline=(ts, te)))  # epoch the EEG data
 
 epochs_combined = mne.concatenate_epochs(epochs_list)  # combine all epochs to one object
-fig = epochs_combined.plot()  # manually mark channels and epochs as bad
+reject_criteria = dict(eeg=100e-6)  # 100 µV
+_ = epochs_combined.drop_bad(reject=reject_criteria)  # automatically drop epochs based on PPA
 
 s = epochs_combined['33285'].average()  # average all shock trials
 v = epochs_combined['33286'].average()  # average all vibrate trials
 b = epochs_combined['32775'].average()  # average all baseline trials
 
-# mne.viz.plot_compare_evokeds(s, combine='mean') for average signal across all channels
-# s.plot_joint() for inclusion of montage topography with signal
-# s.plot() just shows all the signal with all the channels
+evokeds = dict(shock=s, vibrate=v, baseline=b)
+mne.viz.plot_compare_evokeds(evokeds, combine='mean')
