8c017ede73a3c580ed42dc892f2952b6d61b49c3
BCI Lab <ergo2.ncsu@gmail.com>
2022-05-16 14:29:54 -0400

    Variety of changes, mainly adding os.join() calls to data path section, expanding the bandpass filter to .1-30Hz, and correcting the baseline to -100-0ms rather than the whole epoch.
    Also added a test ERP analysis from online, with little success.

--- Data Analysis/ERP_analyze.py
+++ Data Analysis/ERP_analyze.py
@@ -1,19 +1,23 @@
 import os
 import mne
 import channels
+import numpy as np
+import matplotlib.pyplot as plt
+
+#################
+# Pre-Processing:
 
 stims = {
-    '32769': 'OVTK_StimulationId_ExperimentStart',
-    '33285': 'OVTK_StimulationId_Target',
-    '33286': 'OVTK_StimulationId_NonTarget',
-    '32775': 'OVTK_StimulationId_BaselineStart'
+    '33285': 'OVTK_StimulationId_Target',  # shock
+    '33286': 'OVTK_StimulationId_NonTarget',  # vibrate
+    '32775': 'OVTK_StimulationId_BaselineStart'  # baseline
 }
 
 # Establish paths for raw GDF files based on subject and condition of interest
-data_path = os.path.dirname(os.path.realpath(__file__))[:-13] + "Experiment Code\\Data"  # path to GDF files
+data_path = os.path.dirname(os.path.realpath(__file__))[:-13] + os.path.join("Experiment Code", "Data")  # GDF file path
 subject = "sub1"  # format is sub# (1-N)
-condition = "attend"  # or distract
-fnames = [data_path + "\\" + subject + "\\" + subject + "-" + condition + "-" + str(i+1) + ".gdf" for i in range(6)]
+condition = "attend"  # either attend or distract
+fnames = [os.path.join( data_path, subject, subject + "-" + condition + "-" + str(i+1) + ".gdf") for i in range(6)]
 
 amp_order = sum([channels.labels["UB20160321"], channels.labels["UB20141008"],  # order amplifiers as OpenViBE did
                  channels.labels["UB20141007"], channels.labels["UB20110715"]], [])  # (see subject summaries)
@@ -40,18 +44,66 @@ for f in fnames:
     for event in events:
         event[0] = event[0] - offset
 
-    raw.filter(l_freq=0.1, h_freq=10.0)  # bandpass filter 0.5Hz-10Hz
+    raw.filter(l_freq=0.1, h_freq=30.0)  # bandpass filter 0.1Hz-30Hz
 
     epochs_list.append(mne.Epochs(raw, events, event_id=temp_id, tmin=ts, tmax=te,
-                                  preload=True, baseline=(ts, te)))  # epoch the EEG data
+                                  preload=True, baseline=(None, 0)))  # epoch the EEG data, 100ms baseline correction
 
 epochs_combined = mne.concatenate_epochs(epochs_list)  # combine all epochs to one object
 reject_criteria = dict(eeg=100e-6)  # 100 µV
 _ = epochs_combined.drop_bad(reject=reject_criteria)  # automatically drop epochs based on PPA
+# fig = epochs_combined.plot()  # manually drop epochs and channels
 
-s = epochs_combined['33285'].average()  # average all shock trials
-v = epochs_combined['33286'].average()  # average all vibrate trials
-b = epochs_combined['32775'].average()  # average all baseline trials
 
-evokeds = dict(shock=s, vibrate=v, baseline=b)
-mne.viz.plot_compare_evokeds(evokeds, combine='mean')
+################
+# Test analysis: First, the example provided by https://neurokit2.readthedocs.io/en/latest/studies/erp_gam.html
+# Selecting relevant channels (somatosensory cortex)
+picks = ["C3", "C1", "Cz", "C2", "C4"]
+epochs_combined = epochs_combined.pick_channels(picks)
+
+# Transform each condition (stimulus) to array
+condition1 = np.mean(epochs_combined["33285"].get_data(), axis=1)  # shock
+condition2 = np.mean(epochs_combined["33286"].get_data(), axis=1)  # vibrate
+condition3 = np.mean(epochs_combined["32775"].get_data(), axis=1)  # baseline
+
+# Permutation test to find significant cluster of differences
+t_vals, clusters, p_vals, h0 = mne.stats.permutation_cluster_test([condition1, condition2, condition3],
+                                                                  out_type='mask', seed=111)
+
+# Visualize
+fig, (ax0, ax1, ax2) = plt.subplots(nrows=3, ncols=1, sharex=True)
+
+times = epochs_combined.times
+ax0.axvline(x=0, linestyle="--", color="black")
+ax0.plot(times, np.mean(condition1, axis=0), label="Shock")
+ax0.plot(times, np.mean(condition2, axis=0), label="Vibrate")
+ax0.plot(times, np.mean(condition3, axis=0), label="Baseline")
+ax0.legend(loc="upper right")
+ax0.set_ylabel("uV")
+
+# Difference
+ax1.axvline(x=0, linestyle="--", color="black")
+ax1.plot(times, condition1.mean(axis=0) - condition2.mean(axis=0))
+ax1.axhline(y=0, linestyle="--", color="black")
+ax1.set_ylabel("Difference")
+
+# T-values
+ax2.axvline(x=0, linestyle="--", color="black")
+h = None
+for i, c in enumerate(clusters):
+    c = c[0]
+    if p_vals[i] <= 0.05:
+        h = ax2.axvspan(times[c.start],
+                        times[c.stop - 1],
+                        color='red',
+                        alpha=0.5)
+    else:
+        ax2.axvspan(times[c.start],
+                    times[c.stop - 1],
+                    color=(0.3, 0.3, 0.3),
+                    alpha=0.3)
+hf = ax2.plot(times, t_vals, 'g')
+if h is not None:
+    plt.legend((h, ), ('cluster p-value < 0.05', ))
+plt.xlabel("time (ms)")
+plt.ylabel("t-values")
