1bff0fdf03a8c5f67b648ddb81edfd01fd57ed8c
BCI Lab <ergo2.ncsu@gmail.com>
2022-05-23 14:25:19 -0400

    Channel correction now occurs in its own script, which ALSO saves files with the proper BIDS formatted name. ERP analyze no longer does channel renaming and instead loads BIDS formatted EDF files.

---
+++ Data Analysis/Channel Correction.py
@@ -0,0 +1,115 @@
+import os
+import mne
+
+labels = {
+    "UB20160321":
+    [
+        "FC1",
+        "P1",
+        "PO3",
+        "O1",
+        "CP1",
+        "AF3",
+        "Fp1",
+        "Fz",
+        "AF7",
+        "CPz",
+        "FCz",
+        "Cz",
+        "C1",
+        "F1",
+        "AF2",
+        "Fpz"
+    ],
+
+    "UB20141008":
+    [
+        "F7",
+        "FT7",
+        "T7",
+        "TP7",
+        "P7",
+        "F5",
+        "FC5",
+        "C5",
+        "PO7",
+        "P3",
+        "CP3",
+        "FC3",
+        "C3",
+        "F3",
+        "P5",
+        "CP5"
+    ],
+
+    "UB20141007":
+    [
+        "TP8",
+        "P8",
+        "O10",
+        "Iz",
+        "O9",
+        "Oz",
+        "POz",
+        "Pz",
+        "T8",
+        "FT8",
+        "F8",
+        "C6",
+        "CP6",
+        "P6",
+        "PO8",
+        "AF8"
+    ],
+
+    "UB20110715":
+        [
+        "F4",
+        "P2",
+        "CP2",
+        "C2",
+        "FC2",
+        "F2",
+        "AF4",
+        "Fp2",
+        "O2",
+        "PO4",
+        "P4",
+        "CP4",
+        "C4",
+        "FC4",
+        "FC6",
+        "F6",
+    ]
+}
+
+subjects = ["sub2"]  # format is sub# (1-N)
+conditions = ["attend", "distract"]  # both possible attention conditions
+
+# Establish paths for raw GDF files based on subject and condition of interest
+data_path = os.path.dirname(os.path.realpath(__file__))[:-13] + os.path.join("Experiment Code", "Data")  # GDF file path
+
+amp_order = sum([labels["UB20160321"], labels["UB20141008"],  # order amplifiers as OpenViBE did
+                 labels["UB20141007"], labels["UB20110715"]], [])  # (see subject summaries)
+fnames = []
+
+for s in subjects:
+    for condition in conditions:
+        # build a list of all 6 runs for each sub-condition pair
+        fnames = fnames + [os.path.join(data_path, s, s + "-" + condition + "-" + str(i + 1) + ".gdf") for i in range(6)]
+        for f in fnames:
+            raw = mne.io.read_raw_gdf(f)  # load our GDF file
+
+            # Correct channel names using amp_order
+            channel_names = {raw.ch_names[i]: amp_order[i] for i in range(len(amp_order))}  # both should be of length 64
+            raw.rename_channels(channel_names)  # happens in place
+
+            for i in channel_names.values():
+                raw.set_channel_types({i: "eeg"})  # ensure that every channel is set to type "eeg"
+
+            # Grab the data that we need for BIDS formatting
+            run = f[-5:][:-4]
+            num = s[-1:]
+            task = condition[0].upper()
+            BIDS_name = "sub-0" + num + "_task-" + task + "_run-0" + run + "_eeg.edf"
+            raw.export(BIDS_name)  # export new .edf file
--- Data Analysis/ERP_analyze.py
+++ Data Analysis/ERP_analyze.py
@@ -1,8 +1,5 @@
 import os
 import mne
-import channels
-import numpy as np
-import matplotlib.pyplot as plt
 
 #################
 # Pre-Processing:
@@ -13,30 +10,24 @@ stims = {
     '32775': 'OVTK_StimulationId_BaselineStart'  # baseline
 }
 
-# Establish paths for raw GDF files based on subject and condition of interest
+# Establish paths for EDF files based on subject and condition of interest
 data_path = os.path.dirname(os.path.realpath(__file__))[:-13] + os.path.join("Experiment Code", "Data")  # GDF file path
-subjects = ["sub1", "sub2"]  # format is sub# (1-N)
-condition = "attend"  # either attend or distract
+subjects = ["1", "2"]
+condition = "A"  # either Attend or Distract
 
-amp_order = sum([channels.labels["UB20160321"], channels.labels["UB20141008"],  # order amplifiers as OpenViBE did
-                 channels.labels["UB20141007"], channels.labels["UB20110715"]], [])  # (see subject summaries)
 montage = mne.channels.make_standard_montage('standard_1020')
 
 epochs_list = []
 ts = -0.1  # epoch start time
-te = 0.3  # epoch end time
+te = 0.5  # epoch end time
 
 for s in subjects:  # collapse across all subjects (still with just 1 condition, attend or distract)
-    fnames = [os.path.join(data_path, s, s + "-" + condition + "-" + str(i + 1) + ".gdf") for i in range(6)]
+    filename = "sub-0" + s + "_task-" + condition + "_run-0"  # define BIDS filename here for simplicity
+    fnames = [os.path.join(data_path, "sub-0" + s, "eeg", filename + str(i + 1) + "_eeg.edf") for i in range(6)]
+
     for f in fnames:
-        raw = mne.io.read_raw_gdf(f, preload=True)  # load our GDF file
-
-        # Correct channel names using amp_order
-        channel_names = {raw.ch_names[i]: amp_order[i] for i in range(len(amp_order))}  # both should be of length 64
-        raw.rename_channels(channel_names)  # happens in place
-
+        raw = mne.io.read_raw_edf(f, preload=True)  # load our GDF file
         raw.set_montage(montage)  # add standard 10-20 montage information to GDF file
-
         # Pull the event markers from the data's annotations
         events, temp_id = mne.events_from_annotations(raw)
 
@@ -46,14 +37,21 @@ for s in subjects:  # collapse across all subjects (still with just 1 condition,
             event[0] = event[0] - offset
 
         raw.filter(l_freq=0.1, h_freq=40.0)  # bandpass filter 0.1Hz-40Hz
-
         epochs_list.append(mne.Epochs(raw, events, event_id=temp_id, tmin=ts, tmax=te,
-                                      preload=True, baseline=(None, 0)))  # epoch the EEG data, 100ms baseline correction
+                                      preload=True, baseline=(None, 0)))  # epoch the data w/ 100ms baseline correction
 
 epochs_combined = mne.concatenate_epochs(epochs_list)  # combine all epochs to one object
 reject_criteria = dict(eeg=100e-6)  # 100 µV
 _ = epochs_combined.drop_bad(reject=reject_criteria)  # automatically drop epochs based on PPA
-# fig = epochs_combined.plot()  # manually drop epochs and channels
 
 epochs_combined["33285"].average().plot_joint()  # shock
 epochs_combined["33286"].average().plot_joint()  # vibrate
+"""
+# ICA test
+orig = raw.copy()
+ica = mne.preprocessing.ICA(n_components=20, random_state=97, max_iter=800)
+ica.fit(raw)
+ica.plot_sources(raw, show_scrollbars=False)
+#ica.apply(raw)
+ica.plot_overlay(raw, exclude=ica.exclude)
+"""
